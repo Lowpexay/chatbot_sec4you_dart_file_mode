e1514fdcc84e6412342c56c6ad9954b8952e5d9c
Gabriel Gramacho <ggramacho09@gmail.com>
2025-06-17 21:26:52 -0300

    emergency commit

--- chatbot_sec4you/lib/users_map_screen.dart
+++ chatbot_sec4you/lib/users_map_screen.dart
@@ -13,7 +13,7 @@ class UsersMapScreen extends StatelessWidget {
       appBar: AppBar(title: const Text('<Mapa de Usuários./>')),
       body: Stack(
         children: [
-          // Mapa dos usuários
+          // Mapa dos usuárioss
           Positioned.fill(
             child: StreamBuilder<QuerySnapshot>(
               stream: FirebaseFirestore.instance.collection('user_locations').snapshots(),
